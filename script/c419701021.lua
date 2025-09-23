c17858d3a37bc71971c672ea693a20d321fd5427
KSB-Custom <124850393+KSB-Custom@users.noreply.github.com>
2025-09-23 16:43:50 -0600

    Add files via upload

--- script/c419701021.lua
+++ script/c419701021.lua
@@ -2,8 +2,6 @@
 --GENESYS FORMAT
 local s,id=GetID()
 function s.initial_effect(c)
-	local s,id=GetID()
-function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
