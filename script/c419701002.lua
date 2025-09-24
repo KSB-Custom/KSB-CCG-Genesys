9465346aad991f5ccdb6661006e221c325906363
KSB-Custom <124850393+KSB-Custom@users.noreply.github.com>
2025-09-24 01:33:29 -0600

    Add files via upload

--- script/c419701002.lua
+++ script/c419701002.lua
@@ -2,16 +2,17 @@
 --GENESYS FORMAT
 local s,id=GetID()
 function s.initial_effect(c)
-	--Apply the effect of 1 "Killer Tune" monster in your GY that activates if sent to the GY as Synchro Material
+	--Apply these effects, in sequence, also you cannot Special Summon for the rest of this turn after this card resolves, except Tuners
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCountLimit(1,id)
-	e1:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return rp==1-tp end)
-	e1:SetCost(s.applycost)
-	e1:SetTarget(s.applytg)
-	e1:SetOperation(s.applyop)
+	e1:SetCountLimit(1,{id,2})
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	e1:SetHintTiming(0,TIMING_STANDBY_PHASE|TIMING_MAIN_END|TIMINGS_CHECK_MONSTER_E)
 	c:RegisterEffect(e1)
 	--Set this card
 	local e2=Effect.CreateEffect(c)
@@ -27,80 +28,6 @@ function s.initial_effect(c)
 	e2:SetOperation(s.setop)
 	c:RegisterEffect(e2)
 end
-function s.rmfilter(c,e,tp)
-	if not (c:IsSetCard(SET_PRANK_KIDS) and c:IsMonster() and c:IsAbleToRemoveAsCost()) then return false end
-	local effs={c:GetOwnEffects()}
-	for _,eff in ipairs(effs) do
-		if eff:GetCode()==EVENT_BE_MATERIAL then
-			local tg=eff:GetTarget()
-			if tg==nil or tg(eff,tp,Group.CreateGroup(),PLAYER_NONE,0,e,REASON_EFFECT,PLAYER_NONE,0) then
-				return true
-			end
-		end
-	end
-	return false
-end
-function s.applycost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.rmfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local rc=Duel.SelectMatchingCard(tp,s.rmfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp):GetFirst()
-	Duel.Remove(rc,POS_FACEUP,REASON_COST)
-	local available_effs={}
-	local effs={rc:GetOwnEffects()}
-	for _,eff in ipairs(effs) do
-		if eff:GetCode()==EVENT_BE_MATERIAL then
-			local tg=eff:GetTarget()
-			if tg==nil or tg(eff,tp,Group.CreateGroup(),PLAYER_NONE,0,e,REASON_EFFECT,PLAYER_NONE,0) then
-				table.insert(available_effs,eff)
-			end
-		end
-	end
-	e:SetLabelObject(available_effs)
-end
-function s.applytg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then
-		local eff=e:GetLabelObject()
-		return eff and eff:GetTarget() and eff:GetTarget()(e,tp,eg,ep,ev,re,r,rp,0,chkc)
-	end
-	if chk==0 then return true end
-	local eff=nil
-	local available_effs=e:GetLabelObject()
-	if #available_effs>1 then
-		local available_effs_desc={}
-		for _,eff in ipairs(available_effs) do
-			table.insert(available_effs_desc,eff:GetDescription())
-		end
-		local op=Duel.SelectOption(tp,table.unpack(available_effs_desc))
-		eff=available_effs[op+1]
-	else
-		eff=available_effs[1]
-	end
-	Duel.Hint(HINT_OPSELECTED,1-tp,eff:GetDescription())
-	e:SetLabel(eff:GetLabel())
-	e:SetLabelObject(eff:GetLabelObject())
-	e:SetProperty(eff:IsHasProperty(EFFECT_FLAG_CARD_TARGET) and EFFECT_FLAG_CARD_TARGET or 0)
-	local tg=eff:GetTarget()
-	if tg then
-		tg(e,tp,eg,ep,ev,re,r,rp,1)
-	end
-	eff:SetLabel(e:GetLabel())
-	eff:SetLabelObject(e:GetLabelObject())
-	e:SetLabelObject(eff)
-	Duel.ClearOperationInfo(0)
-end
-function s.applyop(e,tp,eg,ep,ev,re,r,rp)
-	local eff=e:GetLabelObject()
-	if not eff then return end
-	e:SetLabel(eff:GetLabel())
-	e:SetLabelObject(eff:GetLabelObject())
-	local op=eff:GetOperation()
-	if op then
-		op(e,tp,Group.CreateGroup(),PLAYER_NONE,0,e,REASON_EFFECT,PLAYER_NONE)
-	end
-	e:SetLabel(0)
-	e:SetLabelObject(nil)
-end
---
 function s.illusionconfilter(c)
 	return c:IsLevelAbove(5) and c:IsSetCard(SET_PRANK_KIDS) and c:IsFaceup() and (c:IsType(TYPE_SYNCHRO) or c:IsType(TYPE_FUSION))
 end
@@ -121,5 +48,104 @@ function s.setop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetValue(LOCATION_REMOVED)
 		e1:SetReset(RESET_EVENT|RESETS_REDIRECT)
 		c:RegisterEffect(e1)
+	end
+end
+--
+function s.applyfilter(c,e,tp)
+	if not (c:IsSetCard(SET_PRANK_KIDS) and c:IsMonster() and c:IsFaceup()) then return false end
+	if c:IsAbleToHand() then return true end
+	local effs={c:GetOwnEffects()}
+	for _,eff in ipairs(effs) do
+		if eff:GetCode()==EVENT_BE_MATERIAL then
+			local tg=eff:GetTarget()
+			if tg==nil or tg(eff,tp,Group.CreateGroup(),PLAYER_NONE,0,e,REASON_EFFECT,PLAYER_NONE,0) then
+				return true
+			end
+		end
+	end
+	return false
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	if chk==0 then return Duel.IsExistingTarget(s.applyfilter,tp,LOCATION_MZONE|LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local tc=Duel.SelectTarget(tp,s.applyfilter,tp,LOCATION_MZONE|LOCATION_GRAVE,0,1,1,nil,e,tp):GetFirst()
+	local available_effs={}
+	local effs={tc:GetOwnEffects()}
+	for _,eff in ipairs(effs) do
+		if eff:GetCode()==EVENT_BE_MATERIAL then
+			local tg=eff:GetTarget()
+			if tg==nil or tg(eff,tp,Group.CreateGroup(),PLAYER_NONE,0,e,REASON_EFFECT,PLAYER_NONE,0) then
+				table.insert(available_effs,eff)
+			end
+		end
+	end
+	local eff=nil
+	if #available_effs>1 then
+		local available_effs_desc={}
+		for _,eff in ipairs(available_effs) do
+			table.insert(available_effs_desc,eff:GetDescription())
+		end
+		local op=Duel.SelectOption(tp,table.unpack(available_effs_desc))
+		eff=available_effs[op+1]
+	else
+		eff=available_effs[1]
+	end
+	if eff then
+		Duel.Hint(HINT_OPSELECTED,1-tp,eff:GetDescription())
+		Duel.ClearTargetCard()
+		tc:CreateEffectRelation(e)
+		e:SetLabel(eff:GetLabel())
+		e:SetLabelObject(eff:GetLabelObject())
+		local tg=eff:GetTarget()
+		if tg then
+			tg(e,tp,eg,ep,ev,re,r,rp,1)
+			eff:SetLabel(e:GetLabel())
+			eff:SetLabelObject(e:GetLabelObject())
+			Duel.ClearOperationInfo(0)
+		end
+		e:SetLabelObject(eff)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,tc,1,tp,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local _,tc=Duel.GetOperationInfo(0,CATEGORY_TOHAND)
+	tc=tc:GetFirst()
+	if tc and tc:IsRelateToEffect(e) then
+		local te=e:GetLabelObject()
+		if te then
+			local break_chk=false
+			--Apply its effect that activates if sent to the GY as Synchro Material
+			local op=te:GetOperation()
+			if tc:IsFaceup() and op then
+				e:SetLabel(te:GetLabel())
+				e:SetLabelObject(te:GetLabelObject())
+				op(e,tp,eg,ep,ev,re,r,rp)
+				break_chk=true
+			end
+			e:SetLabel(0)
+			e:SetLabelObject(nil)
+		end
+		--Return it to the hand
+		if break_chk then Duel.BreakEffect() end
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+	end
+	if not e:IsHasType(EFFECT_TYPE_ACTIVATE) then return end
+	--You cannot Special Summon for the rest of this turn after this card resolves, except Tuners
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(s.splimit)
+	e1:SetReset(RESET_PHASE|PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+function s.splimit(e,c)
+	if c:IsMonster() then
+		return not c:IsSetCard(SET_PRANK_KIDS)
+	elseif c:IsMonsterCard() then
+		return not c:IsSetCard(SET_PRANK_KIDS)
 	end
 end
--- script/c419701035.lua
+++ script/c419701035.lua
@@ -15,6 +15,7 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_BE_MATERIAL)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
 	e1:SetCountLimit(1,{id,2})
@@ -55,14 +56,15 @@ function s.spfilter(c,e,tp)
 end
 function s.thop2(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	local g2=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_DECK|LOCATION_HAND,0,g:GetFirst(),e,tp)
 	if tc:IsRelateToEffect(e) and
-		Duel.SendtoHand(tc,nil,REASON_EFFECT)~=0
-	and #g2>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
-		and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
-		Duel.BreakEffect()
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local sg=g2:Select(tp,1,1,nil)
-		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)~=0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0  then
+		local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_DECK|LOCATION_HAND,0,nil,e,tp)
+		if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local sg=g:Select(tp,1,1,nil)
+			if #sg==0 then return end
+			Duel.BreakEffect()
+			Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+		end
 	end
 end
