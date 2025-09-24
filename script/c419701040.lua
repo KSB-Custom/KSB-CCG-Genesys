f7bffd61be5fdc1af5536c27e8373b967def5d28
KSB-Custom <124850393+KSB-Custom@users.noreply.github.com>
2025-09-24 12:57:11 -0600

    Add files via upload

---
+++ script/c419701040.lua
@@ -0,0 +1,60 @@
+--Prank-Kids Starsies
+--GENESYS FORMAT
+local s,id=GetID()
+function s.initial_effect(c)
+	--fusion summon
+	c:EnableReviveLimit()
+	Fusion.AddProcMixN(c,true,true,aux.FilterBoolFunctionEx(Card.IsSetCard,SET_PRANK_KIDS),2)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetRange(LOCATION_EXTRA)
+	e1:SetCondition(s.hspcon)
+	e1:SetTarget(s.hsptg)
+	e1:SetOperation(s.hspop)
+	c:RegisterEffect(e1)
+	--Fusion Summon 1 "Vaylantz" Fusion Monster
+	local fusparams={aux.FilterBoolFunction(Card.IsSetCard,SET_PRANK_KIDS),nil,s.fextra}
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetCountLimit(1,{id,2})
+	e3:SetTarget(Fusion.SummonEffTG(table.unpack(fusparams)))
+	e3:SetOperation(Fusion.SummonEffOP(table.unpack(fusparams)))
+	c:RegisterEffect(e3)
+end
+function s.fextra(e,tp,mg)
+	return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToGrave),tp,LOCATION_DECK,0,nil)
+end
+function s.extratg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,0,tp,LOCATION_HAND|LOCATION_DECK|LOCATION_MZONE)
+end
+function s.hspfilter(c,tp,sc)
+	return c:IsSetCard(SET_PRANK_KIDS) and Duel.GetLocationCountFromEx(tp,tp,c,sc)>0
+end
+function s.hspcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	return Duel.CheckReleaseGroup(tp,s.hspfilter,1,false,1,true,c,tp,nil,nil,nil,tp,c)
+end
+function s.hsptg(e,tp,eg,ep,ev,re,r,rp,chk,c)
+	local g=Duel.SelectReleaseGroup(tp,s.hspfilter,1,1,false,true,true,c,tp,nil,false,nil,tp,c)
+	if g then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+	return true
+	end
+	return false
+end
+function s.hspop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	Duel.Release(g,REASON_COST|REASON_MATERIAL)
+	g:DeleteGroup()
+end
